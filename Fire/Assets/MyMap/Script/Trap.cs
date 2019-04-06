08a0be5ecb6c7925c112423f9885f8c3bac2f17d
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-04-06 21:43:05 +0900

    함정
    거리로 체킹

--- Fire/Assets/MyMap/Script/Trap.cs
+++ Fire/Assets/MyMap/Script/Trap.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
     public Transform target;
+    public GameObject Enemy;
 
     public float distance;
     public int angle;
+    public float checkDuration;
 
     private void Start()
     {
-        //StartCoroutine(Checking(distance, angle));
+        StartCoroutine(Checking(checkDuration));
+    }
+
+    private IEnumerator Checking(float duration)
+    {
+        while (true)
+        {
+            var f = Vector3.Distance(target.position, transform.position);
+            Debug.Log(f);
+            if (f < distance)
+            {
+                TrapOn();
+                yield break;
+            }
+            yield return new WaitForSeconds(duration);
+        }
+    }
+
+    private void TrapOn()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject.Instantiate(Enemy, transform.position, Quaternion.identity);
+        }
     }
 }
