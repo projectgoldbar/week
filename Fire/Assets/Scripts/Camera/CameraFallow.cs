3c78762d398eaa958f8ab26f7c630efe84f2e47b
vhstm2 <vhstm2@users.noreply.github.com>
2019-04-14 21:05:26 +0900

    플레이어 진행방향

--- Fire/Assets/Scripts/Camera/CameraFallow.cs
+++ Fire/Assets/Scripts/Camera/CameraFallow.cs
@@ -5,7 +5,6 @@ public class CameraFallow : MonoBehaviour
 {
     public Transform target;
     private Vector3 offset = Vector3.zero;
-    public float damp = 5f;
 
     private Coroutine StopRutine;
     public Action rotatestop = () => { };
@@ -25,7 +24,7 @@ public class CameraFallow : MonoBehaviour
         var Move_vec = new Vector3(
             target.localPosition.x,
             target.localPosition.y + offset.y,
-            target.localPosition.z + (Vector3.forward * 3.0f).z);
+            target.localPosition.z);
 
         transform.position = Move_vec;
     }
