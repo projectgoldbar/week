e6605ac7952e48608b25c67d22997ac0f24721c3
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-04-30 21:49:16 +0900

    [UI] 작동법 띄우기 추가
    [UI] 작동법 띄우기 추가

---
+++ Fire/Assets/ZumZumZombie/KM/Scripts/HowtoMoveUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HowtoMoveUI : MonoBehaviour
+{
+    public RawImage howtoMoveUI;
+    Color howtoMoveUIColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        howtoMoveUIColor = howtoMoveUI.color;
+        StartCoroutine("DisappearHowtoMoveUI");
+    }
+
+    IEnumerator DisappearHowtoMoveUI(){
+
+        for (float a = 1.5f; a >= 0; a -= 0.01f)
+        {
+            howtoMoveUI.color = new Vector4(howtoMoveUIColor.r, 
+                                            howtoMoveUIColor.g,
+                                            howtoMoveUIColor.b,
+                                            a);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
