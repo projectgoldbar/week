13d80748c71f9d639d3bd9d52155a29149e73b5f
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-05-01 01:08:17 +0900

    [UI] UI 설명 뜨는 시간 변경
    [UI] UI 설명 뜨는 시간 변경

--- Fire/Assets/ZumZumZombie/KM/Scripts/HowtoMoveUI.cs
+++ Fire/Assets/ZumZumZombie/KM/Scripts/HowtoMoveUI.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class HowtoMoveUI : MonoBehaviour
 {
+    public float disappearTimes = 2.0f;
     public RawImage howtoMoveUI;
     private Color howtoMoveUIColor;
     private WaitForFixedUpdate fixedUpdate = new WaitForFixedUpdate();
@@ -16,7 +17,7 @@ public class HowtoMoveUI : MonoBehaviour
 
     private IEnumerator DisappearHowtoMoveUI()
     {
-        for (float a = 1.5f; a >= 0; a -= 0.01f)
+        for (float a = disappearTimes; a >= 0; a -= 0.01f)
         {
             howtoMoveUI.color = new Vector4(howtoMoveUIColor.r,
                                             howtoMoveUIColor.g,
