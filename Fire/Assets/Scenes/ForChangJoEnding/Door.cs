dbd6311268a5860191abb45edf032f7931bff701
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-05 17:33:20 +0900

    [UI] prefabs_color, layout
    [UI] prefabs_color, layout

---
+++ Fire/Assets/Scenes/ForChangJoEnding/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private Animator doorAnim;
+
+
+    void Start()
+    {
+        doorAnim = this.GetComponent<Animator>();
+    }
+
+
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CheckPlayerIn(other))
+        {
+            CloseDoor();
+        }
+}
+
+
+
+    bool CheckPlayerIn(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void CloseDoor()
+    {
+        doorAnim.SetTrigger("PlayerIn");
+    }
+}
