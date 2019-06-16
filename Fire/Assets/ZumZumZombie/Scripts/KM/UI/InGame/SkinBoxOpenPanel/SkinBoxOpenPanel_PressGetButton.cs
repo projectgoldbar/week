f769988326315e971b035144b58b51c2c329dba1
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-17 01:38:03 +0900

    [UI] SkinBoxPanel 2차 완 : flow 완성
    [UI] SkinBoxPanel 2차 완 : flow 완성
    // 스킨박스 유무 &  언제패널이 켜졌는지에 따라 계속
    1. 스킨박스를 계속 열을지,
    2. 게임 종료 패널로 넘길지,
    3. 패널이 닫히는지
    결정되어야 한다.

---
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/InGame/SkinBoxOpenPanel/SkinBoxOpenPanel_PressGetButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class SkinBoxOpenPanel : MonoBehaviour
+{
+    public void OnPressGetBox_Button()
+    {
+        //if ()
+        //{
+        //    //스킨박스가 남아있다면,
+        //}
+        //else if //게임중이라면,
+
+        //else //종료되서 문제되었었다면,
+        Close();
+    }
+}
