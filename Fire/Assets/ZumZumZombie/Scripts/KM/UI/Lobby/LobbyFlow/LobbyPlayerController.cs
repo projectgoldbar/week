aed6b394057a982854333566c37485dcaaa813cc
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-21 13:30:40 +0900

    [UI][Lobby][화면전환연출]_플레이어 뛰어나가기
    [UI][Lobby][화면전환연출]_플레이어 뛰어나가기

--- Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/LobbyFlow/LobbyPlayerController.cs
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/LobbyFlow/LobbyPlayerController.cs
@@ -4,24 +4,61 @@ using UnityEngine;
 
 public class LobbyPlayerController : MonoBehaviour
 {
-    [SerializeField]
+    public Vector3[] path;
+
     private Animator lobbyPlayerAnim;
+
+    private List<Vector3> listPath;
 
     private void Start()
     {
         lobbyPlayerAnim = GetComponent<Animator>();
+        listPath = new List<Vector3>();
+        SettingPaths();
+    }
+
+    private void SettingPaths()
+    {
+        listPath.Add(transform.position);
+        listPath.Add(transform.position);
+        for (int i = 0; i < path.Length; ++i)
+        {
+            listPath.Add(path[i]);
+        }
+        listPath.Add(path[path.Length - 1]);
     }
 
     public void FlowBeforePlay()
     {
+        ResetAnim();
         RunAnim();
+        RunMove();
+    }
+
+    private void ResetAnim()
+    {
+        //lobbyPlayerAnim.
     }
 
     private void RunAnim()
     {
-        //Debug.Log("b");
-        //lobbyPlayerAnim.SetBool("Static_b", true);
-        //lobbyPlayerAnim.SetFloat("Speed_f", 1.0f);
         lobbyPlayerAnim.SetTrigger("LobbyRun");
+    }
+
+    private void RunMove()
+    {
+        if (LeanTween.isTweening(gameObject))
+        {
+            Debug.Log("tweening");
+            return;
+        }
+        Debug.Log("b");
+        LTDescr d = LeanTween.moveSpline(gameObject, listPath.ToArray(), 3.0f).setOrientToPath(true).setEase(LeanTweenType.easeInQuad);
+        d.setOnComplete(RunMoveComplete);
+    }
+
+    private void RunMoveComplete()
+    {
+        Debug.Log("End");
     }
 }
