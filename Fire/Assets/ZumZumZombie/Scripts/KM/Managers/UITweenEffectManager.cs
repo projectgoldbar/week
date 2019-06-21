5fdc470f05be7faabf7370d2483943194dfb9cda
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-22 05:35:53 +0900

    [UI][InGame] FadeIn
    [UI][InGame] FadeIn
    FadeInImageComplete() 여기 안에 게임 시작하도록 하는거 적기?

--- Fire/Assets/ZumZumZombie/Scripts/KM/Managers/UITweenEffectManager.cs
+++ Fire/Assets/ZumZumZombie/Scripts/KM/Managers/UITweenEffectManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UITweenEffectManager : MonoBehaviour
 {
@@ -19,4 +20,47 @@ public class UITweenEffectManager : MonoBehaviour
     public StageOpenPanel stageOpenPanel;
     public SkinBoxOpenPanel skinBoxOpenPanel;
     public GameOverPanel gameOverPanel;
+    public GameObject FadeInImageObj;
+
+    private float duration = 3.0f;
+    private float from = 255.0f;
+    private float to = 0.0f;
+    private Color beforeFAdeInColor;
+    private Color nowColor;
+
+    private void Start()
+    {
+        resetPanel();
+        startGame();
+    }
+
+    private void resetPanel()
+    {
+        beforeFAdeInColor.a = from;
+        FadeInImageObj.GetComponent<Image>().color = beforeFAdeInColor;
+    }
+
+    public void startGame()
+    {
+        FadeInEffect();
+    }
+
+    private void FadeInEffect()
+    {
+        var d = LeanTween.value(from, to, duration);
+        d.setOnUpdate(x => { ValueUpdateFadeOut(x); });
+        d.setOnComplete(FadeInImageComplete);
+    }
+
+    private void ValueUpdateFadeOut(float value)
+    {
+        nowColor.a = value / 255f;
+        FadeInImageObj.GetComponent<Image>().color = nowColor;
+    }
+
+    private void FadeInImageComplete()
+    {
+        Debug.Log("FadeInEnd");
+        //게임시작 부르기!
+    }
 }
