e1626ab8fadc18a1529307e4eae713f7baa48bb1
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-26 11:07:20 +0900

    [UI][Lobby] 전환효과 2차 : [Lobby]1. 눌러야 하는거 위치들 표기해 주기 _ 효과 프로토구현
    10:~11:05 : [Lobby]1. 눌러야 하는거 위치들 표기해 주기 _ 효과 프로토구현

--- Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/PingPong/ScalePingPong.cs
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/PingPong/ScalePingPong.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScalePingPong : MonoBehaviour
 {
-    private void OnEnable()
+    private float durationT = 2f;
+    private float delayT = 10f;
+
+    private Color nowColor;
+    private float onAlpha;
+    private float offAlpha = 0;
+
+    private void Start()
     {
-        LeanTween.scale(gameObject, Vector3.one * 1.1f, 0.6f).setEase(LeanTweenType.easeInOutQuad).setLoopPingPong();
-        //LeanTween.alpha(gameObject, 0f, 0.6f).setEase(LeanTweenType.easeInOutQuad).setLoopPingPong();
+        nowColor = gameObject.GetComponent<Image>().color;
+        onAlpha = nowColor.a * 255f;
+
+        Debug.Log(onAlpha + " : " + gameObject.GetComponent<Image>().color.a);
+        LeanTween.scale(gameObject, Vector3.one * 1.1f, durationT).setEase(LeanTweenType.easeOutQuart).setLoopClamp();
+        LTDescr d = LeanTween.value(gameObject, onAlpha, offAlpha, durationT).setLoopClamp();
+        d.setOnUpdate(x => { valueUpdateAlpha(x); });
+    }
+
+    private void valueUpdateAlpha(float value)
+    {
+        nowColor.a = value / 255f;
+        gameObject.GetComponent<Image>().color = nowColor;
     }
 }
