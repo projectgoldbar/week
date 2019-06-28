eb152a02d8b1af4597173469ea0c37646f7dfdd7
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-28 23:35:24 +0900

    --
    --

--- Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/Panel/SkinPanel.cs
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/Panel/SkinPanel.cs
@@ -2,13 +2,23 @@
 
 public class SkinPanel : MonoBehaviour
 {
+    private Vector2 closePos = new Vector2(0f, 1000f);
+    private Vector2 openePos = new Vector2(0f, 0f);
+
+    private void Start()
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = closePos;
+    }
+
     public void OpenPanel()
     {
         gameObject.SetActive(true);
+        gameObject.GetComponent<RectTransform>().anchoredPosition = openePos;
     }
 
     public void ClosePanel()
     {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = closePos;
         gameObject.SetActive(false);
     }
 }
--- Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/Panel/StatPanel.cs
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/Panel/StatPanel.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class StatPanel : MonoBehaviour
 {
+    private Vector2 closePos = new Vector2(0f, 1000f);
+    private Vector2 openePos = new Vector2(0f, 0f);
+
+    private void Start()
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = closePos;
+    }
+
     public void OpenPanel()
     {
         gameObject.SetActive(true);
+        gameObject.GetComponent<RectTransform>().anchoredPosition = openePos;
     }
 
     public void ClosePanel()
     {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = closePos;
         gameObject.SetActive(false);
     }
 }
