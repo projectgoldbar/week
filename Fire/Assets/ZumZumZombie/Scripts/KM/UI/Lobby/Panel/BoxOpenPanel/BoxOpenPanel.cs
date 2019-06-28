8f137dd56e54e25848ed85f5b5b7bfe8e96cde1c
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-06-28 17:25:41 +0900

    1. [UI][Lobby][Box] : popUp_박스 1개 열기
    1. [UI][Lobby][Box] : popUp_박스 1개 열기

---
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/Panel/BoxOpenPanel/BoxOpenPanel.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class BoxOpenPanel : MonoBehaviour
+{
+    public GameObject boxLayer;
+
+    public GameObject boxObj;
+    public GameObject boxOpenObj;
+
+    public GameObject openBoxButton;
+    public GameObject getBoxButton;
+
+    private float IntroTime = 0.5f;
+
+    private float rotTime = 2.0f;
+    private float scaleTime = 1.5f;
+
+    private Quaternion boxRotQ;
+    private Vector3 targetBoxScaleforIntro;
+
+    private Vector2 openPostion = new Vector2(0, 0);
+    private Vector2 closePostion = new Vector2(2500, 0);
+
+    private void Awake()
+    {
+        boxLayer.SetActive(true);
+
+        targetBoxScaleforIntro = boxObj.GetComponent<Transform>().localScale;
+        boxRotQ = boxObj.GetComponent<Transform>().rotation;
+    }
+
+    private void Start()
+    {
+        Close();
+    }
+
+    public void Close()
+    {
+        //gameObject.SetActive(false);
+        gameObject.GetComponent<RectTransform>().anchoredPosition = closePostion;
+        boxObj.SetActive(false);
+        boxOpenObj.SetActive(false);
+    }
+
+    public void Open()
+    {
+        //gameObject.SetActive(true);
+        gameObject.GetComponent<RectTransform>().anchoredPosition = openPostion;
+
+        boxObj.SetActive(true);
+        boxOpenObj.SetActive(false);
+
+        resetPanel();
+        PlayTweenEffectforOpen();
+    }
+
+    private void PlayTweenEffectforOpen()
+    {
+        IntroBox();
+    }
+
+    private void IntroBox_Complete()
+    {
+        openBoxButton.SetActive(true);
+    }
+
+    private void resetPanel()
+    {
+        boxObj.GetComponent<Transform>().localScale = Vector3.zero;
+        boxObj.GetComponent<Transform>().rotation = boxRotQ;
+        openBoxButton.SetActive(false);
+        getBoxButton.SetActive(false);
+    }
+
+    private void IntroBox()
+    {
+        LTDescr decr = LeanTween.scale(boxObj, targetBoxScaleforIntro, IntroTime).setEase(LeanTweenType.easeOutElastic);
+        decr.setOnComplete(IntroBox_Complete);
+    }
+}
---
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/Panel/BoxOpenPanel/BoxOpenPanel_PressOpenButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class BoxOpenPanel : MonoBehaviour
+{
+    public void OnPressOpenBox_Button()
+    {
+        if (LeanTween.isTweening(boxObj))
+        {
+            Debug.Log("박스도는중");
+            return;
+        }
+
+        RotBoxTweenEffectForButton();
+    }
+
+    private void RotBoxTweenEffectForButton_Complete()
+    {
+        OpentSkinBox();
+    }
+
+    //-----------
+
+    private void RotBoxTweenEffectForButton()
+    {
+        var d = LeanTween.rotate(boxObj, Vector3.up * 1080f * 3, rotTime).setEase(LeanTweenType.easeOutExpo);
+        d.setOnComplete(RotBoxTweenEffectForButton_Complete);
+    }
+
+    private void OpentSkinBox()
+    {
+        BoomEffect();
+        ChangeMesh();
+        IntroOpenBoxTween();
+    }
+
+    private void BoomEffect()
+    {
+    }
+
+    private void ChangeMesh()
+    {
+        boxObj.SetActive(false);
+        boxOpenObj.SetActive(true);
+
+        openBoxButton.SetActive(false);
+        getBoxButton.SetActive(true);
+    }
+
+    private void IntroOpenBoxTween()
+    {
+        LTDescr decr = LeanTween.scale(boxOpenObj, targetBoxScaleforIntro * 0.9f, IntroTime).setEase(LeanTweenType.easeOutElastic)
+            .setLoopPingPong();
+    }
+}
