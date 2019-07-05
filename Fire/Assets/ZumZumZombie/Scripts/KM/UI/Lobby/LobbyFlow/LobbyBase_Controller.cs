8cdf9f6241c4c146a9ace23f2f3dcd8ed3401b4d
imitme_GGA6th <37390101+imitme@users.noreply.github.com>
2019-07-05 16:56:47 +0900

    -UI 문 테두리 색 변경 : [UI][Lobby] = [화면전환연출] 부가작업 (3차)  색변경 작업 전
    -UI 문 테두리 색 변경 : [UI][Lobby] = [화면전환연출] 부가작업 (3차)  색변경 작업 전

--- Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/LobbyFlow/LobbyBase_Controller.cs
+++ Fire/Assets/ZumZumZombie/Scripts/KM/UI/Lobby/LobbyFlow/LobbyBase_Controller.cs
@@ -1,10 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyBase_Controller : MonoBehaviour
 {
     public LobbyPlayerController lobbyPlayerController;
     public DoorController doorController;
     public StartObjController StartObjController;
+
+    /*
+   public GameObject mainCameraBgforMood;
+   public GameObject dLightforMood;
+   public GameObject rawImageforMood;
+
+   private Color mainCameraBgforMoodColor;
+   private Color dLightforMoodColor;
+   private Color rawImageforMoodColor;
+
+   private float changeMoodTimeSpan = 2.0f;
+   private float changeMoodTime = 2.0f;
+
+   private Color currColor = Color.white;
+
+   private void Awake()
+   {
+       mainCameraBgforMoodColor = mainCameraBgforMood.GetComponent<Camera>().backgroundColor;
+       dLightforMoodColor = dLightforMood.GetComponent<Light>().color;
+       //rawImageforMoodColor = rawImageforMood.GetComponent<Renderer>().material.
+   }
+
+   private void Start()
+   {
+       Debug.Log(rawImageforMoodColor);
+       // StartCoroutine("ChangeMoodColor");
+   }
+
+   private IEnumerator ChangeMoodColor()
+   {
+       float progress = 0;
+       float increment = changeMoodTime / changeMoodTimeSpan;
+
+       while (progress < 1)
+       {
+           mainCameraBgforMoodColor = Color.Lerp(Color.red, Color.blue, progress);
+           progress += increment;
+           Debug.Log(mainCameraBgforMoodColor);
+           yield return new WaitForSeconds(changeMoodTime);
+       }
+   }
+
+   private void Update()
+   {
+       float progress = 0;
+       float increment = changeMoodTime / changeMoodTimeSpan;
+
+       while (progress < 1)
+       {
+           mainCameraBgforMoodColor = Color.Lerp(Color.red, Color.blue, progress);
+           progress += increment;
+           Debug.Log(mainCameraBgforMoodColor);
+       }
+   }
+   */
 }
