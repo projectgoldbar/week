fbb149ca3cd04052bc402449392fee0bdbce0aca
wkdeo12 <43337006+wkdeo12@users.noreply.github.com>
2019-07-13 23:04:18 +0900

    금괴

--- Fire/Assets/ZumZumZombie/zz_etc/Coin.cs
+++ Fire/Assets/ZumZumZombie/zz_etc/Coin.cs
@@ -4,14 +4,16 @@ public class Coin : MonoBehaviour
 {
     public Vector3 RotationSpeed = new Vector3(0f, 0, 100f);
     public int coinSection;
+    public Material coinMaterial;
     private ParticlePool particlePool;
-    private SectorManager sectorManager;
+
+    //private SectorManager sectorManager;
     public bool coinRotate = true;
 
     private void Awake()
     {
         particlePool = FindObjectOfType<ParticlePool>();
-        sectorManager = FindObjectOfType<SectorManager>();
+        //sectorManager = FindObjectOfType<SectorManager>();
     }
 
     private void OnEnable()
@@ -19,6 +21,10 @@ public class Coin : MonoBehaviour
         //섹터로 처리하지 않아서 주석처리했음
         //sectorManager.sectors[coinSection].currentCoin++;
         coinRotate = true;
+    }
+
+    public void ColorChange()
+    {
     }
 
     protected virtual void Update()
